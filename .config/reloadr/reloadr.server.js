2e99f9152cc9f576b2aaf7100ef6d32064f34151
Sergej Samsonenko <contact@sergej.codes>
2020-09-12 12:27:00 +0200

    added chalk and adjusted reloadr for better log

--- .config/reloadr/reloadr.server.js
+++ .config/reloadr/reloadr.server.js
@@ -1,17 +1,26 @@
 const http = require('http')
 const WebSocket = require('ws')
+const chalk = require('chalk')
 const options = require('../reloadr.config.js')
+
+/**
+ * custom console.log
+ */
+const log = (msg) => {
+  console.log(`${chalk.yellow.bold('[reloadr]')} ${chalk.yellow.inverse(` ${msg} `)}`)
+}
 
 /**
  * create websocket server
  * for remote shopify theme
  */
 const wss = new WebSocket.Server({ port: options.websoketPort })
-const clients = []
 
-wss.on('connection', (ws, req) => {
-  clients.push(ws)
-})
+wss.broadcast = (msg) => {
+  wss.clients.forEach((client) => {
+    client.send(msg)
+  })
+}
 
 /**
  * create http server
@@ -20,10 +29,13 @@ wss.on('connection', (ws, req) => {
 http.createServer((req, res) => {
   const url = req.url
 
-  if (url === '/reload' && clients.length) {
+  if (url === '/reload' && wss.clients.size) {
     setTimeout(() => {
-      clients.forEach(client => client.send('reload'))
+      wss.broadcast('reload')
+      log('reloading clients')
     }, options.delay)
+  } else {
+    log('no clients connected')
   }
 
   res.end()
