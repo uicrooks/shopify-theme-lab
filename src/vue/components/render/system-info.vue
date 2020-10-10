ff345e274f9df8a81444665a971427c26f659a15
Sergej Samsonenko <contact@sergej.codes>
2020-10-10 13:02:58 +0200

    added filename annotation

--- src/vue/components/render/system-info.vue
+++ src/vue/components/render/system-info.vue
@@ -17,6 +17,12 @@
 
 <script>
 export default {
+  /**
+   * if a name is defined, you can use the component as <system-info></system-info> in .liquid files
+   * otherwise the component will be named after the file path starting from components/ directory
+   * for this file it is: <render-system-info></render-system-info>
+   */
+  // name: 'systemInfo',
   props: {
     shopifyData: {
       type: Object,
