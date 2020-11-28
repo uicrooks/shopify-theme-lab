c560bfb3d8c4aee0c0734c09d1164b3a064143ca
Sergej Samsonenko <contact@sergej.codes>
2020-11-28 23:41:42 +0100

    reloadr now tries to reconnect on lost connection

--- .config/reloadr/reloadr.client.js
+++ .config/reloadr/reloadr.client.js
@@ -1,4 +1,5 @@
 const options = require('./reloadr.config.js')
+let initialConnectionClose = true // allows to display disonnected error only once on 'disconnected' event
 
 initReloadrClient = () => {
   const socket = new WebSocket(`ws://localhost:${options.websocketPort}`)
@@ -6,17 +7,32 @@ initReloadrClient = () => {
   /**
    * custom console.log
    */
-  const log = (msg) => {
-    const css = 'background: rgb(80,90,115); color: rgb(200,200,255)'
+  const log = ({ msg, type }) => {
+    let css
+
+    if (type === 'success') css = 'background: rgb(190,255,175); color: rgb(60,100,40)'
+    else if (type === 'error') css = 'background: rgb(245,165,165); color: rgb(80,40,40)'
+
     console.log(`%c Reloadr: ${msg} `, css)
   }
 
   /**
    * websocket eventslisteners
    */
-  socket.addEventListener('open', () => log('connected'))
-  socket.addEventListener('error', () => log('error'))
-  socket.addEventListener('close', () => log('disonected'))
+  socket.addEventListener('open', () => {
+    if (!initialConnectionClose) initialConnectionClose = true
+    log({ msg: 'connected', type: 'success' })
+  })
+
+  socket.addEventListener('close', () => {
+    if (initialConnectionClose) {
+      log({ msg: 'disonected', type: 'error' })
+      initialConnectionClose = false
+    }
+
+    setTimeout(() => initReloadrClient(), 1000)
+  })
+
   socket.addEventListener('message', (event) => {
     if(event.data === 'reload') location.reload()
   })
