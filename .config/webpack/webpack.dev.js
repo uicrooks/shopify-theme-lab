124dc5443565df9f044a112fdf94a9849c773562
Sergej Samsonenko <contact@sergej.codes>
2020-11-29 13:57:16 +0100

    added preprocessor support for webpack

--- .config/webpack/webpack.dev.js
+++ .config/webpack/webpack.dev.js
@@ -7,7 +7,7 @@ const log = require('../plugins/log')
  * initial console.log on development start
  */
 log.box({
-  msg: 'Starting development tasks',
+  msg: 'Started development process',
   color: 'green'
 })
 
@@ -32,19 +32,36 @@ module.exports = merge(common, {
           }
         ]
       },
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: require(path.resolve(__dirname, '../postcss.config.js'))
-            }
-          }
+      ... (() => {
+        const rules = []
+
+        const loaders = [
+          { test: /\.css$/i },
+          { test: /\.s[ac]ss$/i, loader: 'sass-loader' },
+          { test: /\.less$/i, loader: 'less-loader' },
+          { test: /\.styl$/i, loader: 'stylus-loader' }
         ]
-      }
+
+        loaders.forEach((element, index) => {
+          rules.push({
+            test: element.test,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: require(path.resolve(__dirname, '../postcss.config.js'))
+                }
+              }
+            ]
+          })
+
+          if (element.loader) rules[index].use.push(element.loader)
+        })
+
+        return rules
+      })()
     ]
   }
 })
--- .config/webpack/webpack.prod.js
+++ .config/webpack/webpack.prod.js
@@ -10,7 +10,7 @@ const log = require('../plugins/log')
  * initial console.log on build start
  */
 log.box({
-  msg: 'Starting building assets',
+  msg: 'Started build process',
   color: 'green'
 })
 
@@ -30,19 +30,36 @@ module.exports = merge(common, {
           plugins: [ '@babel/plugin-transform-runtime' ]
         }
       },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: require(path.resolve(__dirname, '../postcss.config.js'))
-            }
-          }
+      ... (() => {
+        const rules = []
+
+        const loaders = [
+          { test: /\.css$/i },
+          { test: /\.s[ac]ss$/i, loader: 'sass-loader' },
+          { test: /\.less$/i, loader: 'less-loader' },
+          { test: /\.styl$/i, loader: 'stylus-loader' }
         ]
-      }
+
+        loaders.forEach((element, index) => {
+          rules.push({
+            test: element.test,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: require(path.resolve(__dirname, '../postcss.config.js'))
+                }
+              }
+            ]
+          })
+
+          if (element.loader) rules[index].use.push(element.loader)
+        })
+
+        return rules
+      })()
     ]
   },
   plugins: [
