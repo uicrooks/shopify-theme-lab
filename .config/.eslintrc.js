269e3aab3a824e1c40c00db5347ed1f9c09c4e62
Sergej Samsonenko <contact@sergej.codes>
2020-11-30 21:57:45 +0100

    updated .eslintrc.js

--- .config/.eslintrc.js
+++ .config/.eslintrc.js
@@ -1,10 +1,21 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'plugin:vue/recommended'
   ],
   plugins: [
     'vue'
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    commonjs: true,
+    browser: true,
+    es6: true
+  },
   rules: {
     /**
      * add custom rules
