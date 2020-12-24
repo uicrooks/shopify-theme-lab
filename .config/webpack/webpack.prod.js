e2a58161fcaecef27281872e8a605f7b721f132a
Sergej Samsonenko <contact@sergej.codes>
2020-12-24 11:42:13 +0100

    moved common position

--- .config/webpack/webpack.prod.js
+++ .config/webpack/webpack.prod.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
-const common = require('./webpack.common.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin') // included in webpack 5, no need to add to package.json
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
