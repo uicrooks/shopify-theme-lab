682b8ca2bcfbf1949dedb4f35b8adeda4143bbd5
Sergej Samsonenko <contact@sergej.codes>
2021-04-17 14:03:05 +0200

    refactor: vue instance initailization

--- src/main.js
+++ src/main.js
@@ -7,80 +7,84 @@ import axios from 'axios'
 import './css/main.css'
 
 /**
- * create vue instance
+ * create vue instance function
  */
-const app = createApp({})
+const createVueApp = () => {
+  const app = createApp({})
+
+  /**
+   * vue components
+   * auto-import all vue components
+   */
+  const vueComponents = require.context('./vue/components/', true, /\.(vue|js)$/)
+
+  vueComponents.keys().forEach(key => {
+    const component = vueComponents(key).default
+
+    // if a component has a name defined use the name, else use the path as the component name
+    const name = component.name
+      ? component.name
+      : key.replace(/\.(\/|vue|js)/g, '').replace(/(\/|-|_|\s)\w/g, (match) => match.slice(1).toUpperCase())
+
+    app.component(name, component)
+  })
+
+  /**
+   * vuex
+   * auto-import all modules
+   */
+  const vuexModules = require.context('./vue/store/', true, /\.js$/)
+  const modules = {}
+
+  vuexModules.keys().forEach(key => {
+    const name = key.replace(/\.(\/|js)/g, '').replace(/\s/g, '-')
+    modules[name] = vuexModules(key).default
+  })
+
+  const store = createStore({
+    strict: process.env.NODE_ENV !== 'production',
+    modules
+  })
+
+  app.use(store)
+
+  /**
+   * vue mixins
+   * auto-register all mixins with a 'global' keyword in their filename
+   */
+  const mixins = require.context('./vue/mixins/', true, /.*global.*\.js$/)
+
+  mixins.keys().forEach(key => {
+    app.mixin(mixins(key).default)
+  })
+
+  /**
+   * vue directives
+   * auto-register all directives with a 'global' keyword in their filename
+   */
+  const directives = require.context('./vue/directives/', true, /.*global.*\.js$/)
+
+  directives.keys().forEach(key => {
+    const directive = directives(key).default
+    app.directive(directive.name, directive.directive)
+  })
+
+  /**
+   * vue config
+   * extend with additional features
+   */
+  app.config.globalProperties.$axios = axios
+
+  /**
+   * vue plugins
+   * extend with additional features
+   */
+  // app.use(MyPlugin)
+
+  return app
+}
 
 /**
- * vue components
- * auto-import all vue components
+ * create and mount vue instance
  */
-const vueComponents = require.context('./vue/components/', true, /\.(vue|js)$/)
-
-vueComponents.keys().forEach(key => {
-  const component = vueComponents(key).default
-
-  // if a component has a name defined use the name, else use the path as the component name
-  const name = component.name
-    ? component.name
-    : key.replace(/\.(\/|vue|js)/g, '').replace(/(\/|-|_|\s)\w/g, (match) => match.slice(1).toUpperCase())
-
-  app.component(name, component)
-})
-
-/**
- * vuex
- * auto-import all modules
- */
-const vuexModules = require.context('./vue/store/', true, /\.js$/)
-const modules = {}
-
-vuexModules.keys().forEach(key => {
-  const name = key.replace(/\.(\/|js)/g, '').replace(/\s/g, '-')
-  modules[name] = vuexModules(key).default
-})
-
-const store = createStore({
-  strict: process.env.NODE_ENV !== 'production',
-  modules
-})
-
-app.use(store)
-
-/**
- * vue mixins
- * auto-register all mixins with a 'global' keyword in their filename
- */
-const mixins = require.context('./vue/mixins/', true, /.*global.*\.js$/)
-
-mixins.keys().forEach(key => {
-  app.mixin(mixins(key).default)
-})
-
-/**
- * vue directives
- * auto-register all directives with a 'global' keyword in their filename
- */
-const directives = require.context('./vue/directives/', true, /.*global.*\.js$/)
-
-directives.keys().forEach(key => {
-  const directive = directives(key).default
-  app.directive(directive.name, directive.directive)
-})
-
-/**
- * vue config
- * extend with additional features
- */
-app.config.globalProperties.$axios = axios
-
-/**
- * vue plugins
- * extend with additional features
- */
-// app.use(MyPlugin)
-
-/**
- * mount vue instance
- */
-app.mount('#app')
+createVueApp().mount('#app')
