fbafd5bd95a6312da117c6742d40cf3a77b137f6
Sergej Samsonenko <contact@sergej.codes>
2021-04-21 22:55:34 +0200

    remove font file-loader from webpack

--- .config/webpack/webpack.common.js
+++ .config/webpack/webpack.common.js
@@ -35,17 +35,6 @@ module.exports = {
             }
           }
         ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            publicPath: './',
-            outputPath: './'
-          }
-        }
       }
     ]
   },
