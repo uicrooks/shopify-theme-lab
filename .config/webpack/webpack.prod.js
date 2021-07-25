cf290ef7522f54e9c1897abab48da40340c4b6b1
Sergej Samsonenko <contact@sergej.codes>
2021-07-25 16:56:48 +0200

    extract css for dev and prod

--- .config/webpack/webpack.prod.js
+++ .config/webpack/webpack.prod.js
@@ -1,6 +1,4 @@
-const path = require('path')
 const { merge } = require('webpack-merge')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin') // included in webpack 5, no need to add to package.json
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const common = require('./webpack.common.js')
@@ -20,48 +18,9 @@ module.exports = merge(common, {
           presets: [ '@babel/preset-env' ],
           plugins: [ '@babel/plugin-transform-runtime' ]
         }
-      },
-      ... (() => {
-        const rules = []
-
-        const loaders = [
-          { test: /\.(css|postcss)$/i },
-          { test: /\.s[ac]ss$/i, loader: 'sass-loader' },
-          { test: /\.less$/i, loader: 'less-loader' },
-          { test: /\.styl$/i, loader: 'stylus-loader' }
-        ]
-
-        loaders.forEach((element, index) => {
-          rules.push({
-            test: element.test,
-            use: [
-              MiniCssExtractPlugin.loader,
-              'css-loader',
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: require(path.resolve(__dirname, '../postcss.config.js'))
-                }
-              }
-            ]
-          })
-
-          if (element.loader) rules[index].use.push(element.loader)
-        })
-
-        return rules
-      })()
+      }
     ]
   },
-  plugins: [
-    /**
-     * docs: https://webpack.js.org/plugins/mini-css-extract-plugin
-     */
-    new MiniCssExtractPlugin({
-      filename: './bundle.css',
-      chunkFilename: '[id].css'
-    })
-  ],
   optimization: {
     minimize: true,
     minimizer: [
