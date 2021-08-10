b470a0be2ef756c8735d4a56e5a967488c2243aa
weemwum <31605623+weemwum@users.noreply.github.com>
2021-08-10 16:04:29 -0700

    set layout

--- src/layout/account.js
+++ src/layout/account.js
@@ -1,23 +1,12 @@
-import Vue from "vue";
+import {Vue, store} from "../main";
 console.log("layout-account");
 
-const vueComponents = require.context("../vue/templates/account", true, /\.(vue|js)$/);
-vueComponents.keys().forEach(key => {
-  const component = vueComponents(key).default;
-  const name = component.name
-    ? component.name
-    : key.replace(/\.(\/|vue|js)/g, "").replace(/(\/|-|_|\s)\w/g, (match) => match.slice(1).toUpperCase());
-  Vue.component(name, component);
-});
-
-/* Import Filters
-const filters = require.context("./vue/filters/", true, /\.(vue|js)$/);
-filters.keys().forEach(key => {
-  const filterMethod = filters(key);
-  const name = key.replace(/\.(\/|vue|js)/g, "").replace(/(\/|-|_|\s)\w/g, (match) => match.slice(1).toUpperCase());
-  Vue.filter(name, filterMethod);
-});*/
-
 new Vue({
-  el: "#layout-account",
+  el: "#app",
+  components: {
+  },
+  store,
+  mounted() {
+    window.app = this;
+  }
 });
