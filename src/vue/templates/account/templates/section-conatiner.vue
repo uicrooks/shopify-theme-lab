008e0cfbf4294a63995c5030f47e0233e3adc8b2
Alex <Hwang>
2021-09-01 13:27:18 -0700

    lint issues fixed

--- src/vue/templates/account/templates/section-conatiner.vue
+++ src/vue/templates/account/templates/section-conatiner.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="section-container-box-component">
+  <div class="section-container-component">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: "AccountSectionContainerBox"
+  name: "AccountSectionContainer"
 };
 </script>
 
@@ -14,7 +14,7 @@ export default {
 <style scoped lang="scss">
 @import "@/styles/main.scss";
 
-.section-container-box-component {
+.section-container-component {
   background-color: $white;
   border-radius: 5px;
   box-shadow: 0 4px 8px 0 rgb(0 0 0 / 10%);
