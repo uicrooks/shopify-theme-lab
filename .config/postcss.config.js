5346530fb126fd92fa285d6d6286a66a070a4fa7
Sergej Samsonenko <contact@sergej.codes>
2021-09-12 18:14:41 +0200

    feat: require tailwindcss nesting

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss/nesting'),
     require('tailwindcss')(path.resolve(__dirname, '../src/tailwind.config.js')),
     require('autoprefixer')
   ]
