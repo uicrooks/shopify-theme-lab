80650757c69a93a0498f54e20256ce64a4fc88cf
Sergej Samsonenko <contact@sergej.codes>
2021-09-12 18:13:57 +0200

    refactor: remove postcss-preset-env

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -4,11 +4,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss')(path.resolve(__dirname, '../src/tailwind.config.js')),
-    require('autoprefixer'),
-    /**
-     * use tomorrow’s CSS today
-     * docs: https://preset-env.cssdb.org/features
-     */
-    require('postcss-preset-env')({ stage: 1 })
+    require('autoprefixer')
   ]
 }
