81bd5ea87fd043299c7f115a12af3ab6e3857e0a
Tim_Tan <80441177+haotan19@users.noreply.github.com>
2021-12-13 14:58:24 +0800

    update tailwindcss config file

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,23 +4,19 @@
  * docs: https://tailwindcss.com/docs/configuration
  * default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  mode: 'jit',
   theme: {
     extend: {},
     container: {
       center: true,
-      padding: '1rem'
-    }
+      padding: "1rem",
+    },
   },
   plugins: [],
-  content: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      path.resolve(__dirname, '**/*.{js,vue}'),
-      path.resolve(__dirname, '../shopify/**/*.liquid')
-    ]
-  }
-}
+  content: [
+    path.resolve(__dirname, "**/*.{js,vue}"),
+    path.resolve(__dirname, "../shopify/**/*.liquid"),
+  ],
+};
