e81d3a364b66985c00fe04e2087e1f1088e6b8a8
TomJacobsUK <tom@tomjacobs.co.uk>
2022-08-22 17:24:10 +0100

    Enable Vue.js devtools in development env

--- .config/webpack/webpack.prod.js
+++ .config/webpack/webpack.prod.js
@@ -40,5 +40,11 @@ module.exports = merge(common, {
        */
       new CssMinimizerPlugin()
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false
+    })
+  ]
 })
